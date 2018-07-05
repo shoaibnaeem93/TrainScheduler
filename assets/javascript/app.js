cc5ab823c4b20006854736f51c1117d12f6eb05d
Shoaib Naeem <you@example.com>
2018-07-05 11:11:58 -0700

    finished css and html

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAaukQmXWxCLXpHNF1omQMWOFZCb74JLOA",
+    authDomain: "train-scheduler-b954c.firebaseapp.com",
+    databaseURL: "https://train-scheduler-b954c.firebaseio.com",
+    projectId: "train-scheduler-b954c",
+    storageBucket: "",
+    messagingSenderId: "582940945034"
+  };
+  firebase.initializeApp(config);
